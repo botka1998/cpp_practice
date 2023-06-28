67ae17c29f1799dfbaced2c403ab62672bd3fe21
botka1998 <57000683+botka1998@users.noreply.github.com>
2023-06-28 18:22:05 +0200

    implement sigmoid function for 10x10 matix

--- logistic_regression/main.cpp
+++ logistic_regression/main.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
+#include <random>
 #include <gmtl/gmtl.h>
 
 using std::cout;
 using std::endl;
 
+// def sigmoid(z):
+//     s = 1 / (1 + np.exp(-z))
+
+//     return s
+gmtl::Matrix<float, 10, 10> sigmoid(gmtl::Matrix<float, 10, 10> z)
+{
+    for (int row = 0; row < z.Rows; row++)
+    {
+        for (int col = 0; col < z.Cols; col++)
+        {
+            z(row, col) = 1 / (1 + std::exp(-z(row, col)));
+        }
+    }
+    return z;
+}
+
 int main()
 {
     gmtl::Matrix<float, 10, 10> my_matrix;
+
+    gmtl::mult<float, 10, 10>(my_matrix, 0.1);
+
+    my_matrix = sigmoid(my_matrix);
     cout << my_matrix;
     return 1;
 }
