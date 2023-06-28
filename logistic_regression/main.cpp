dfd5f037deb87b88c86a66342603d0352fa5b36a
botka1998 <57000683+botka1998@users.noreply.github.com>
2023-06-28 18:39:13 +0200

    change sigmoid function to non mutating

--- logistic_regression/main.cpp
+++ logistic_regression/main.cpp
@@ -12,15 +12,43 @@ using std::endl;
 template <typename T, unsigned int Rows, unsigned int Cols>
 gmtl::Matrix<T, Rows, Cols> sigmoid(gmtl::Matrix<T, Rows, Cols> &z)
 {
+    /*
+    Compute the sigmoid of z
+
+    Arguments : z-- A Matrix of any size.
+
+        Return : s-- sigmoid(z)
+    */
+    gmtl::Matrix<T, Rows, Cols> resultMatrix;
+
     for (int row = 0; row < z.Rows; row++)
     {
         for (int col = 0; col < z.Cols; col++)
         {
-            z(row, col) = 1 / (1 + std::exp(-z(row, col)));
+            resultMatrix(row, col) = 1 / (1 + std::exp(-z(row, col)));
         }
     }
-    return z;
+    return resultMatrix;
 }
+
+// def initialize_with_zeros(dim):
+//     """
+//     This function creates a vector of zeros of shape (dim, 1) for w and initializes b to 0.
+
+//     Argument:
+//     dim -- size of the w vector we want (or number of parameters in this case)
+
+//     Returns:
+//     w -- initialized vector of shape (dim, 1)
+//     b -- initialized scalar (corresponds to the bias) of type float
+//     """
+//     # YOUR CODE STARTS HERE
+//     w = np.zeros((dim, 1))
+//     b = 0.0
+
+//     # YOUR CODE ENDS HERE
+
+//     return w, b
 
 int main()
 {
@@ -28,7 +56,7 @@ int main()
 
     gmtl::mult<float, 10, 10>(my_matrix, 0.1);
 
-    sigmoid(my_matrix);
+    my_matrix = sigmoid(my_matrix);
     cout << my_matrix;
     return 1;
 }
